b3d37cce049719cd4dfe9f6c2bbb9d62a10cbac0
Lukasz <lukczo@outlook.com>
2020-09-28 12:16:39 +0200

    working on cards

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@Angular/forms'
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './core/navbar/navbar.component';
@@ -7,18 +8,21 @@ import { WelcomeComponent } from './core/welcome/welcome.component';
 import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { StartComponent } from './game/start/start.component';
+import { FlashcardComponent } from './game/flashcard/flashcard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     WelcomeComponent,
-    StartComponent
+    StartComponent,
+    FlashcardComponent
   ],
   imports: [
     BrowserModule,
     RouterModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/game/start/start.component.ts
+++ src/app/game/start/start.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-start',
@@ -6,10 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./start.component.css']
 })
 export class StartComponent implements OnInit {
+  queryInput = null;
+  defInput = null;
+
+  flashCards = [];
 
   constructor() { }
 
   ngOnInit(): void {
   }
+  addFlashCard() {
+    const queri = this.queryInput;
+    const defi = this.defInput;
 
+    this.queryInput = null;
+    this.defInput = null;
+    
+    const flashCard =  {'Query': queri,'Definition': defi}
+    this.flashCards.push(flashCard)
+
+    console.log(this.flashCards);
+  }
 }
