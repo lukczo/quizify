8dafad93ceb554925834e508ceabaa1a93fcafc2
Lukasz <lukczo@outlook.com>
2020-09-28 16:30:06 +0200

    working on game mechanics

--- src/app/game/play/play.component.ts
+++ src/app/game/play/play.component.ts
@@ -10,6 +10,7 @@ export class PlayComponent implements OnInit {
    currentQuery = this.drawQuestion();   
    correctAnswer = null;
    wrongAnswers = 'Wrong';
+   answers = this.shuffleArray(this.yourCards)
  
    constructor( ) {}
 
@@ -23,5 +24,19 @@ export class PlayComponent implements OnInit {
     console.log('drawQuestion:', drawnQ)
     return drawnQ
   }
+  
+  shuffleArray(array): Array {
+    console.log('at start of func:', array);
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
 
+    console.log('at end of func:', array);
+
+    let random = Math.floor(Math.random() * 5);
+
+    array.splice(random, 1, {id: 32344332, Query: 'kurwa', Definition: 'mac'});
+    return array
+}
 }
